e0c5269c21aad74e2dd81fe5aaf60598eeb199f8
bitski <18152029+bitski@users.noreply.github.com>
2026-06-07 22:47:15 +0200

    feat(integration): extend CF7Adapter with BookingService DI and initial hooks/stubs

--- src/integration/CF7Adapter.php
+++ src/integration/CF7Adapter.php
@@ -3,18 +3,59 @@
  * CF7Adapter.
  *
  * Adapter for integrating with CF7 plugin.
+ * Depends on BookingService injected via the constructor.
  *
  * @since 0.1.0
  */
 
 namespace BitskiWPCF7Booking\integration;
 
-class CF7Adapter extends Adapter {
+use BitskiWPCF7Booking\application\BookingService;
+
+class CF7Adapter extends Adapter
+{
+    protected BookingService $bookingService;
+
+    /**
+     * Main class name of the external plugin or module required by this adapter.
+     */
+    protected string $dependencyClass = 'WPCF7';
+
+    /**
+     * Injects dependencies for the CF7Adapter.
+     *
+     * @param  BookingService  $bookingService
+     *
+     * @since 0.1.2
+     */
+    public function __construct(BookingService $bookingService)
+    {
+        $this->bookingService = $bookingService;
+        error_log('CF7Adapter constructor called');
+        error_log('BookingService injected: ' . ($bookingService instanceof BookingService ? 'true' : 'false'));
+    }
+
     /**
      * Registers hooks for the external integration.
      */
     protected function registerHooks(): void
     {
-        // TODO: Implement registerHooks() method.
+        add_action('wpcf7_before_send_mail', [$this, 'handleBookingRequest']);
+        add_filter('wpcf7_posted_data', [$this, 'captureFormPayload']);
+    }
+
+    /**
+     * @since 0.1.2
+     */
+    public function handleBookingRequest()
+    {
+        error_log('Booking request received');
+    }
+
+    /**
+     * @since 0.1.2
+     */
+    public function captureFormPayload() {
+
     }
 }

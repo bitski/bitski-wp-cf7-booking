4fda631de72b3b23e0819610baf4f6f006fdab78
bitski <18152029+bitski@users.noreply.github.com>
2026-06-09 16:12:32 +0200

    feat(booking): rename accepts() to book() and orchestrate booking in BookingService

--- src/application/BookingService.php
+++ src/application/BookingService.php
@@ -4,19 +4,32 @@
  *
  * @since 0.2.1
  */
+
 namespace BitskiWPCF7Booking\application;
 
 use BitskiWPCF7Booking\domain\Reservation;
 
 class BookingService
 {
-    public function __construct() {
+    public function __construct()
+    {
         // TODO: Phase 4:
         // $this->capacityManager = new CapacityManager();
         // Add dependencies here (Repository).
     }
 
-    public function accepts(Reservation $reservation):bool
+    public function book(Reservation $reservation): bool
+    {
+        if ( ! $this->accept($reservation)) {
+            return false;
+        }
+
+        // TODO: Phase 4: ReservationRepository->save($reservation);
+
+        return true;
+    }
+
+    public function accept(Reservation $reservation): bool
     {
         // TODO: Phase 4:
         // if (!capacityManager->isCapacityAvailable($reservation) {

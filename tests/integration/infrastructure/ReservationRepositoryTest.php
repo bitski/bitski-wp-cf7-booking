bed178e3a7c488d5edb9731ad474c05d73663378
bitski <18152029+bitski@users.noreply.github.com>
2026-07-02 02:25:06 +0200

    test(integration): add ReservationRepository test infrastructure

--- tests/integration/infrastructure/ReservationRepositoryTest.php
+++ tests/integration/infrastructure/ReservationRepositoryTest.php
@@ -1,8 +1,32 @@
 <?php
 
+declare(strict_types=1);
+
+namespace BitskiWPCF7Booking\tests\integration\infrastructure;
+
 use PHPUnit\Framework\TestCase;
+
+use BitskiWPCF7Booking\infrastructure\ReservationRepository;
 
 class ReservationRepositoryTest extends TestCase
 {
+    private ReservationRepository $reservationRepository;
 
+    protected function setUp(): void
+    {
+        global $wpdb;
+
+        $this->reservationRepository = new ReservationRepository();
+
+        $tableName = $wpdb->prefix . BITSKI_WP_CF7_BOOKING_TABLE_RESERVATIONS;
+        $wpdb->query("DELETE FROM $tableName");
+    }
+
+    protected function tearDown(): void
+    {
+        global $wpdb;
+
+        $tableName = $wpdb->prefix . BITSKI_WP_CF7_BOOKING_TABLE_RESERVATIONS;
+        $wpdb->query("DELETE FROM $tableName");
+    }
 }

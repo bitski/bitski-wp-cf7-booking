1efc719644411018923d5c555f411142c8eb8b86
bitski <18152029+bitski@users.noreply.github.com>
2026-07-07 00:34:59 +0200

    feat(domain): implement capacity check logic in CapacityManager and integrate ReservationRepository

--- src/domain/CapacityManager.php
+++ src/domain/CapacityManager.php
@@ -2,12 +2,78 @@
 /**
  * Capacity manager.
  *
- * @since 0.4.0
+ * @since 0.4.1
  */
 
 namespace BitskiWPCF7Booking\domain;
 
+use DateInterval;
+use DateMalformedStringException;
+
+use BitskiWPCF7Booking\infrastructure\ReservationRepository;
+
 class CapacityManager
 {
+    private ReservationRepository $reservationRepository;
 
+    public function __construct(ReservationRepository $reservationRepository)
+    {
+        $this->reservationRepository = $reservationRepository;
+    }
+
+    /**
+     *
+     */
+    public function isCapacityAvailable(Reservation $reservation): bool
+    {
+        $overlappingReservations = $this->findOverlappingReservations($reservation);
+
+        $guestCountEvents = $this->createGuestCountEvents($overlappingReservations);
+
+        $highestGuestCount = $this->calculateHighestGuestCount($guestCountEvents);
+
+        if ($highestGuestCount > 20) {
+            return false;
+        }
+
+        return true;
+    }
+
+    /**
+     * Returns an array of overlapping reservations.
+     *
+     * @throws DateMalformedStringException
+     */
+    private function findOverlappingReservations(Reservation $reservation): array
+    {
+        $startAt         = $reservation->getStartAt();
+        $durationMinutes = $reservation->getDurationMinutes();
+        $endAt           = $startAt->add(
+            new DateInterval("PT{$durationMinutes}M")
+        );
+
+        return $this->reservationRepository->findOverlappingReservations($startAt, $endAt);
+    }
+
+    /**
+     *
+     */
+    private function createGuestCountEvents(array $overlappingReservations): array
+    {
+        foreach ($overlappingReservations as $overlappingReservation) {
+            // TODO: Create guest count event.
+        }
+
+        return [];
+    }
+
+    /**
+     *
+     */
+    private function calculateHighestGuestCount(array $guestCountEvents): int
+    {
+        // TODO: Calculate highest guest count.
+
+        return 0;
+    }
 }
